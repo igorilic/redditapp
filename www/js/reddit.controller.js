9e2745bdaa020ed7ffa682cfd74a40d9c75024ec
Igor Ilic <ilicigor@outlook.com>
2016-02-15 00:38:06 +0100

    refresher and infinite-scroll

--- www/js/reddit.controller.js
+++ www/js/reddit.controller.js
@@ -8,13 +8,45 @@
   function RedditController($http, $scope) {
     var vm = this;
     vm.stories = [];
-
-    $http.get('https://www.reddit.com/r/politics/new/.json')
-      .success(function(data){
-        angular.forEach(data.data.children, function(child) {
-          vm.stories.push(child.data);
-        })
+    vm.loadMore = loadMore;
+    vm.doRefresh = doRefresh;
+    ///////////////
+    function loadMore() {
+      var params = {};
+      if (vm.stories.length > 0) {
+        params['after'] = vm.stories[vm.stories.length - 1].name;
+      }
+      loadStories(params, function(olderStories) {
+        vm.stories = vm.stories.concat(olderStories);
+        $scope.$broadcast('scroll.infiniteScrollComplete');
       });
+    }
+
+    function doRefresh() {
+      var params = {
+        'before': vm.stories[0].name
+      };
+      loadStories(params, function(newerStories) {
+        vm.stories = newerStories.concat(vm.stories);
+        $scope.$broadcast('scroll.refreshComplete');
+      });
+    }
+
+    function loadStories(params, callback) {
+
+      $http.get('https://www.reddit.com/r/eu4/new/.json', {params: params})
+        .success(function(data){
+          var stories = [];
+          angular.forEach(data.data.children, function(child) {
+            stories.push(child.data);
+          });
+          callback(stories);
+        });
+    }
+
+    // $scope.$on = ('$stateChangeSuccess', function() {
+    //   $scope.loadMore();
+    // });
 
   }
 }());
